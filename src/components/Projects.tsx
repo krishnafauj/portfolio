9567eb8d7ffbc91fd7b3a6e3a27e611ac6c04e42
Krishna Faujdar <krishnafauj02@gmail.com>
2025-12-30 05:14:57 +0530

    accession changes

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -7,7 +7,7 @@ const Projects = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         {/* Section Title (Added a title for context) */}
         <div className="text-center mb-16">
-           <h2 className="text-3xl  text-gray-900 mb-4">Featured Projects</h2>
+           <h2 className="text-3xl  text-gray-900 mb-4">My Projects</h2>
           <div className="w-20 h-1 bg-blue-600 mx-auto"></div>
         </div>
 
